41253c303668d5f89204ed0fd0e899fd3e770b6a
assiamerzoug <assia.merzoug@cu-barika.dz>
2026-08-20 03:00:11 +1300

    Delete src/generators directory

--- src/generators/SequentialNonce.m
+++
@@ -1,5 +0,0 @@
-function nonce = SequentialGenerateNonce(N)
-
-nonce = uint32(0:N-1);
-
-end
--- src/generators/src/generators/MTGenerateNonce.m
+++
@@ -1,9 +0,0 @@
-function nonce = MTGenerateNonce(N,seed)
-
-rand('twister',seed);
-
-nonce = floor((2^32)*rand(N,1));
-
-nonce = uint32(nonce);
-
-end
